bdb7a2b5f8dbbc01796dedb9c11dd1b10fc8ef17
Alexandr Kozlinskiy <akozlins@gmail.com>
2016-04-08 13:24:41 +0200

    remove "." prefix from files

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -eu -o pipefail
 
-declare -a files=(.conkyrc .dmrc .gitconfig .gmrunrc .gv .inputrc .nethackrc .rootrc \
-                  .xscreensaver)
+declare -a files=(conkyrc dmrc emacs gitconfig gmrunrc gv inputrc nethackrc rootrc \
+                  xinitrc xscreensaver xsession \
+                  Xdefaults)
 
 for file in ${files[@]}
 do
-  [ -f $file ] && ln -sfv ~{/.dotfiles,}/$file
+  [ -f $file ] && ln -sfv ~/.dotfiles/$file ~/.$file
 done
