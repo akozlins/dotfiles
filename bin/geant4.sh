37200d57368d866e0bdbc91d67c9c12aa68aca5a
Alexandr Kozlinskiy <akozlins@gmail.com>
2016-10-04 15:34:00 +0200

    https for submodules, check geant4.sh exists

--- bin/geant4.sh
+++ bin/geant4.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #set -eu -o pipefail
 
-cd /usr/local/bin
-source geant4.sh
-cd - > /dev/null
+if [[ -f /usr/local/bin/geant4.sh ]] ; then
+  cd /usr/local/bin
+  source /usr/local/bin/geant4.sh
+  cd - > /dev/null
+fi
