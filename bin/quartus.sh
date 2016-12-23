9a7bcc43fa837a0f22cc403e52825be22b53f1af
Alexandr Kozlinskiy <akozlins@gmail.com>
2016-12-23 13:51:16 +0100

    add quartus.sh

---
+++ bin/quartus.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export LM_LICENSE_FILE=
+
+ROOT=/usr/local/intelFPGA/16.1
+
+export QUARTUS_ROOTDIR=$ROOT/quartus
+export PATH=$PATH:$QUARTUS_ROOTDIR/bin
+export PATH=$PATH:$QUARTUS_ROOTDIR/sopc_builder/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$QUARTUS_ROOTDIR/linux64
+
+#export QSYS_ROOTDIR=$QUARTUS_ROOTDIR/sopc_builder/bin
+
+export SOPC_KIT_NIOS2=$ROOT/nios2eds
+export PATH=$PATH:$SOPC_KIT_NIOS2/bin
+export PATH=$PATH:$SOPC_KIT_NIOS2/sdk2/bin
+export PATH=$PATH:$SOPC_KIT_NIOS2/bin/gnu/H-x86_64-pc-linux-gnu/bin
