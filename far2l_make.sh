06940e54b31da28afc6f18742e01a0d3f372d2cb
Alexandr Kozlinskiy <akozlins@gmail.com>
2017-11-19 16:18:40 +0100

    update fbpanel and far2l build scripts

--- far2l_make.sh
+++ far2l_make.sh
@@ -5,13 +5,13 @@ cd "$(dirname -- "$(readlink -f -- "$0")")"
 
 if [ ! -d "far2l/.git" ] ; then
     git clone --depth 1 https://github.com/elfmz/far2l
-    patch -p1 < far2l.patch
 fi
 
-cd far2l
-git pull
+git -C far2l stash
+git -C far2l pull
+patch -p0 < far2l.patch
 
-mkdir -p build
-cd build
+mkdir -p far2l/build
+cd far2l/build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j4
--- fbpanel_make.sh
+++ fbpanel_make.sh
@@ -6,12 +6,13 @@ cd "$(dirname -- "$(readlink -f -- "$0")")"
 if [ ! -d "fbpanel/.git" ] ; then
     git clone --depth 1 https://github.com/aanatoly/fbpanel
 #    sed -i 's|python|python2|' configure fbpanel/.config/{repl.py,help,tar.py}
-    patch -p1 < fbpanel.patch
 fi
 
-cd fbpanel
-git pull
+git -C fbpanel stash
+git -C fbpanel pull
+patch -p0 < fbpanel.patch
 
+cd fbpanel
 ./configure --prefix="$HOME/.local"
 make
 make install
