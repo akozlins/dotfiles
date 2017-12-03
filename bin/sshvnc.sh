c633c5afd1033dae633052c7403097051ae5b54d
Alexandr Kozlinskiy <akozlins@gmail.com>
2017-12-03 14:17:30 +0100

    [bin] use RPORT in sshvnc

--- bin/sshvnc.sh
+++ bin/sshvnc.sh
@@ -9,10 +9,12 @@ while true ; do
 done
 echo "PORT = $PORT"
 
+RPORT=5900
+
 X11VNC_CMD="x11vnc -nopw -display :0 -localhost -once -timeout 5 -cursor none -scale 2/3"
 
-ssh -S none -f -L "$PORT":localhost:5900 -o ExitOnForwardFailure=yes "$@" "$X11VNC_CMD"
+ssh -S none -f -L "$PORT:localhost:$RPORT" -o ExitOnForwardFailure=yes "$@" "$X11VNC_CMD"
 nc -z localhost "$PORT"
 sleep 2.5; vncviewer DotWhenNoCursor=1 localhost:"$PORT"
 
-ssh "$@" -O cancel -L "$PORT":localhost:5900
+ssh "$@" -O cancel -L "$PORT:localhost:$RPORT"
