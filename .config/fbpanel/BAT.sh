637ef7ea3a48f67a5a3b73c5b37791e0a94aa4f5
Alexandr Kozlinskiy <akozlins@gmail.com>
2018-03-24 14:10:58 +0100

    [fbpanel] fix, n/a if no BAT*

--- .config/fbpanel/BAT.sh
+++ .config/fbpanel/BAT.sh
@@ -2,6 +2,7 @@
 set -eu
 
 for BAT in /sys/class/power_supply/BAT* ; do
+    [ ! -e "$BAT" ] && echo "n/a" && exit 1
     energy_now="$(cat $BAT/energy_now)"
     energy_full="$(cat $BAT/energy_full_design)"
     echo $(($energy_now/($energy_full/100)))%
