f0f30b38ded990dcb6fc9a9abc5f77dbabad733d
Alexandr Kozlinskiy <akozlins@gmail.com>
2018-07-26 09:22:15 +0200

    [sshvnc] LC_ALL=C, gen random password

--- bin/sshvnc.sh
+++ bin/sshvnc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euf
 
+export LC_ALL=C
+
 while
     PORT=$(hexdump -n 2 -e '/2 "%u"' /dev/urandom)
     [ "$PORT" -lt 16384 ] || nc -z localhost "$PORT"
@@ -9,7 +11,10 @@ echo "PORT = $PORT"
 
 RPORT=5900
 
+PASSWD=$(tr -dc a-zA-Z0-9 < /dev/urandom | head -c16)
+
 X11VNC_CMD=(x11vnc
+    -passwd "$PASSWD"
     -localhost -once -timeout 5
     -display :0
     -cursor none -scale 2/3
@@ -20,6 +25,6 @@ LFWD="$PORT:localhost:$RPORT"
 ssh -S none -f -L "$LFWD" -o ExitOnForwardFailure=yes "$@" -- "$(printf '%q ' "${X11VNC_CMD[@]}")"
 nc -z localhost "$PORT"
 sleep 2.5
-vncviewer DotWhenNoCursor=1 localhost:"$PORT"
+vncviewer DotWhenNoCursor=1 localhost:"$PORT" -passwd <(vncpasswd -f <<< "$PASSWD")
 
 ssh -O cancel -L "$LFWD" "$@"
