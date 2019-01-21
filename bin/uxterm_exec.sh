04b3c2937336527aed15d00be948579009f7b05c
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-01-21 16:13:33 +0100

    [uxterm_exec] refactor

--- bin/uxterm_exec.sh
+++ bin/uxterm_exec.sh
@@ -1,27 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 set -euf
 
 if [ $# -ne 2 ] ; then
-  echo "usage: $0 geometry program"
-  exit 1
+    echo "usage: $0 <geometry> <command>"
+    exit 1
 fi
 
+umask 077
 tmp=$(mktemp)
 cleanup() {
     rv=$?
-    rm -rf -- "$tmp"
+    rm -f -- "$tmp"
     exit $rv
 }
-trap "cleanup" EXIT
+trap cleanup EXIT
 
 geometry=$1
-program="rm -f -- '$tmp' ; $2"
+command="rm -f -- '$tmp' ; $2"
 
-/usr/bin/uxterm -geometry "$geometry" -e "sh -c '$program'" > /dev/null 2>&1 &
+/usr/bin/uxterm -geometry "$geometry" -e /bin/sh -c "$command" > /dev/null 2>&1 &
 
 printf "wait for '%s' " "$tmp"
 while [ -f "$tmp" ] ; do
     printf "."
-    sleep 0.01
+    sleep 0.1
 done
 printf " [ok]\\n"
