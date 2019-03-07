59934c3dcdb1187a8a7a40fdd89c984976b14c2c
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-03-07 23:21:38 +0100

    [tmux] mv plugins and resurrect to ~/.cache/tmux

--- .config/tmux/init.sh
+++ .config/tmux/init.sh
@@ -3,6 +3,11 @@ set -euf
 unset CDPATH
 cd "$(dirname -- "$(readlink -e -- "$0")")" || exit 1
 
+mkdir -vp ~/.cache/tmux/plugins
+ln -vsf ~/.cache/tmux/plugins
+mkdir -vp ~/.cache/tmux/resurrect
+ln -vsf ~/.cache/tmux/resurrect
+
 # TODO: add version
 repos=(
     "https://github.com/tmux-plugins/tpm"
@@ -16,5 +21,5 @@ for repo in "${repos[@]}" ; do
         rm -rf -- "$name"
         git clone "$repo"
     fi
-    ( cd "$name" ; git fetch --verbose --prune )
+    ( cd "$name" ; git fetch --verbose )
 done
