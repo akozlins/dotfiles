cd61bcb7531016a66b84c9b7420be8d58471a754
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-03-12 09:49:02 +0100

    [far2l] fix roots

--- .config/far2l/roots.sh
+++ .config/far2l/roots.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 paste <(cat << EOF
+$2
 $HOME
 /run/media/$USER
 /
 EOF
 ) <(cat << EOF | column -t -s '\t' -R 1
+$2
 $HOME
 /run/media/$USER
 /
 EOF
 ) <(cat << EOF
+ &-
  &H:
  &M:
  &Z:
