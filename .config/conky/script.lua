dd3d8ddc60d6b28cdbc784d342a87887a2736c1c
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-03-26 11:04:24 +0100

    [conky] use ~=

--- .config/conky/script.lua
+++ .config/conky/script.lua
@@ -5,7 +5,7 @@ do
         local f = io.popen('ip link show up')
         for line in f:lines() do
             local name = line:match("^[0-9]+: ([^:]+):")
-            if name and not ( name == 'lo' ) then
+            if name and name ~= 'lo' then
                 local s = conky_parse(string.format("  %s: ${upspeedf %s} KiB / ${downspeedf %s} KiB", name, name, name))
                 if string.len(r) > 0 then r = r .. '\n' end
                 r = r .. s
