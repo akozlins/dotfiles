6755652a0995298ceff16dcf86bcd03ddcce7166
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-05-19 11:46:22 +0200

    [bin] update template

--- bin/template.sh
+++ bin/template.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -euf
+IFS="$(printf '\n\t')"
 unset CDPATH
 cd "$(dirname -- "$(readlink -e -- "$0")")" || exit 1
 
-#set -o errexit
-#set -o nounset
-#IFS=$'\n\t'
+#set -o errexit -o nounset
