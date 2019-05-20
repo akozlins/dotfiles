f8b630ee56c4d21afa1f7e5401cc418d122b6deb
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-05-20 19:19:55 +0200

    [sbin] improve git-ls-blobs

--- sbin/git-ls-blobs.sh
+++ sbin/git-ls-blobs.sh
@@ -1,9 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+set -euf
 
-objects=$(git rev-list --all --objects)
-
-git cat-file --batch-all-objects --batch-check | grep blob | sort -k3nr \
-| while read -r sha1 type size ; do
-    name=$(echo "$objects" | grep "$sha1" | awk '{print $2}')
-    echo "$sha1 $size $name"
-done
+join \
+    <(git cat-file --batch-all-objects --batch-check | sort) \
+    <(git rev-list --all --objects | sort) \
+    | sort -nrk3
