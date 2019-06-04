91c2c2d78cfd69ea7edb76dce793e6c78ca26ea5
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-06-04 09:23:01 +0200

    [far2l] custom open.sh - use 'case $mime in'

--- .config/far2l/open.sh
+++ .config/far2l/open.sh
@@ -1,1 +1,25 @@
 #!/bin/sh
+set -euf
+
+mime=$(xdg-mime query filetype "$1")
+
+case "$mime" in
+    application/pdf)
+        exec evince "$1"
+        ;;
+    audio/*)
+        exec vlc "$1"
+        ;;
+    image/*)
+        exec geeqie "$1"
+        ;;
+    video/*)
+        exec vlc "$1"
+        ;;
+    text/*)
+        exec gedit "$1"
+        ;;
+    *)
+        exec zenity --error --text="unknown mime type: '$mime'"
+        ;;
+esac
