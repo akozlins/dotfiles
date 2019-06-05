81f54f9b9b7ed3cff32b858ae90af9c3508790d3
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-06-05 11:26:25 +0200

    [bin] lock - handle light-locker

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -euf
 
-if [ ! -z "$(pidof /usr/bin/xscreensaver)" ] ; then
+if [ -n "$(pidof /usr/bin/xscreensaver)" ] ; then
     exec xscreensaver-command -lock
+fi
+
+if [ -n "$(pidof /usr/bin/light-locker)" ] ; then
+    exec light-locker-command -l
 fi
 
 exit 1
