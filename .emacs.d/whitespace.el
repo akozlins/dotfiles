519d36d5177570b95b77e2780c53feac6f631302
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-10-02 16:18:54 +0200

    [emacs] gray whitespaces

--- .emacs.d/whitespace.el
+++ .emacs.d/whitespace.el
@@ -1,14 +1,22 @@
 ;
 
+; https://www.emacswiki.org/emacs/WhiteSpace
+; https://www.gnu.org/software/emacs/manual/html_node/elisp/Face-Attributes.html
+
 (require 'whitespace)
 (setq whitespace-style '(
-    space-mark
-    newline-mark
-    tab-mark
+    face
+    spaces tabs newline
+    space-mark newline-mark tab-mark
 ))
 (setq whitespace-display-mappings '(
     (space-mark         ?\x20           [?\xB7]         [?.]            )
     (newline-mark       ?\n             [?\xB6 ?\n]     [?$ ?\n]        )
     (tab-mark           ?\t             [?\xBB ?\t]     [?\\ ?\t]       )
 ))
+
+(set-face-attribute 'whitespace-space nil :background nil :foreground "#CCCCCC")
+(set-face-attribute 'whitespace-tab nil :background nil :foreground "#CCCCCC")
+(set-face-attribute 'whitespace-newline nil :background nil :foreground "#CCCCCC")
+
 (global-whitespace-mode)
