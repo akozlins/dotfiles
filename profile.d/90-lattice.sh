1e551ec043b5378f6fd1ea888e64451d11501030
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-04-29 23:25:47 +0200

    [lattice] def LATTICE_LICENSE_FILE/_DIAMOND_HOME

--- profile.d/90-lattice.sh
+++ profile.d/90-lattice.sh
@@ -1,5 +1,8 @@
 #
 
+export LATTICE_LICENSE_FILE="$XDG_CONFIG_HOME/LatticeSemi/license.dat"
+export LATTICE_DIAMOND_HOME="/usr/local/diamond/3.11_x64"
+
 # /etc/udev/rules.d/10-lattice.rules
 #SUBSYSTEM=="usb",ATTRS{idVendor}=="0403",ATTRS{idProduct}=="6010",GROUP="users",MODE="0666",SYMLINK+="ftdi-%n"
 #SUBSYSTEM=="usb",ATTRS{idVendor}=="0403",ATTRS{idProduct}=="6010",RUN+="/bin/sh -c 'basename %p > /sys/bus/usb/drivers/ftdi_sio/unbind'"
