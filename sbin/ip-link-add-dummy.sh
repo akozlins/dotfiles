3671c4648bd3b230768ccd871b7eea2c7f169864
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-05-01 15:08:16 +0200

    [sbin] add ip-link-add-dummy

---
+++ sbin/ip-link-add-dummy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -euf
+
+if [ "$(id -u)" -ne 0 ] ; then
+    exec sudo "$0" "$@"
+fi
+
+MYNAME=bond0
+MYMAC=
+
+while [[ $# -gt 0 ]] ; do
+    case $1 in
+    --name)
+        shift
+        MYNAME=$2
+        shift
+        ;;
+    --mac)
+        shift
+        MYMAC=$2
+        shift
+        ;;
+    --delete)
+        ip link delete "$MYNAME" type dummy
+        rmmod dummy
+        exit 0
+        ;;
+    *)
+        exit 1
+    esac
+done
+
+modprobe dummy
+ip link add "$MYNAME" type dummy
+
+if [ -n "${MYMAC:+x}" ] ; then
+    ifconfig "$MYNAME" hw ether "$MYMAC"
+fi
