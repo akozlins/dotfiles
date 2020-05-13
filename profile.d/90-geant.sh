63189da88fd8996342762e6151affbc568c9bf01
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-05-13 18:04:14 +0200

    [profile] update geant4.sh

--- profile.d/90-geant.sh
+++ profile.d/90-geant.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-if [ -f "/usr/local/bin/geant4.sh" ] && cd "/usr/local/bin" ; then
-    . "geant4.sh"
-    cd - > /dev/null
-fi
+for MY_GEANT4_SH in "/usr/local/bin/geant4.sh" "$HOME/.local/bin/geant4.sh" ; do
+    if [ -f "$MY_GEANT4_SH" ] && cd "$(dirname "$MY_GEANT4_SH")" ; then
+        . "$MY_GEANT4_SH"
+        cd - > /dev/null
+        break
+    fi
+done
