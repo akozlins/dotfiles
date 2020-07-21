b1e0e1ede72ce70bbe49007de3c7426e33f662c1
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-07-21 13:04:49 +0200

    prefer xsecurelock

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -euf
 
+if [ -x "/usr/bin/xsecurelock" ] ; then
+    export XSECURELOCK_BLANK_TIMEOUT=1
+    exec /usr/bin/xsecurelock
+fi
+
 if [ -n "$(pidof /usr/bin/xscreensaver)" ] ; then
     exec xscreensaver-command -lock
 fi
 
 if [ -n "$(pidof /usr/bin/light-locker)" ] ; then
     exec light-locker-command -l
-fi
-
-if [ -e "/usr/bin/xsecurelock" ] ; then
-    export XSECURELOCK_BLANK_TIMEOUT=1
-    exec /usr/bin/xsecurelock
 fi
 
 exit 1
