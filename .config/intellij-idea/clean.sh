de056e7118999f519977bb68fc55d3a7c778e8bd
akozlins <>
2020-08-26 14:19:25 +0200

    [idea] update .gitignore files and clean script

--- .config/intellij-idea/clean.sh
+++ .config/intellij-idea/clean.sh
@@ -8,4 +8,8 @@ set -x
 git clean -Xdf -- .
 
 ( cd "$DOTFILES/.java" || exit 1 ; git clean -Xdf -- . )
+( cd "$DOTFILES/.config/intellij-idea" || exit 1 ; git clean -Xdf -- . )
+
 rm -rfv -- "$XDG_CACHE_HOME/intellij-idea"
+rm -rfv -- "$XDG_CONFIG_HOME/JetBrains"
+rm -rfv -- "$XDG_DATA_HOME/JetBrains"
