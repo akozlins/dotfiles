929da6eb6dee27121d53aa36826911252a6de35a
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-09-10 10:57:11 +0200

    [profile] mv geant.sh -> cern.sh, set root path

---
+++ profile.d/90-cern.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+for MY_GEANT4_SH in \
+    "/usr/local/bin/geant4.sh" \
+    "$HOME/.local/bin/geant4.sh" \
+    "$HOME/.local/geant4.cern.ch/bin/geant4.sh" \
+; do
+    if [ -f "$MY_GEANT4_SH" ] && cd "$(dirname -- "$MY_GEANT4_SH")" ; then
+        . "$(readlink -f -- "$MY_GEANT4_SH")"
+        cd - > /dev/null
+        break
+    fi
+done
+
+unset -v MY_GEANT4_SH
+
+if [ -x "$HOME/.local/root.cern.ch/bin/root" ] ; then
+    PATH="$HOME/.local/root.cern.ch/bin:$PATH"
+fi
