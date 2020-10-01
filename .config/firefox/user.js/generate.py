da1aecb0109f29e4b51b447d636be99b92365e43
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-10-01 20:39:52 +0200

    [ff] refactor generate.py

--- .config/firefox/user.js/generate.py
+++ .config/firefox/user.js/generate.py
@@ -5,10 +5,14 @@ import os, subprocess;
 def read_prefs(prefs, f) :
     def user_pref(key, value) :
         prefs[key] = value;
+    # js uses false/true and null
     false = False;
     true = True;
     null = None;
-    exec(subprocess.check_output(f"cat {f} | gcc -E -P -", shell = True, text = True));
+    stream = open(f);
+    # run gcc preprocessor (remove comments, etc.)
+    output = subprocess.check_output("gcc -E -P -", stdin=stream, shell=True);
+    exec(output);
 
 FIREFOX_HOME = os.path.normpath(os.path.dirname(os.path.realpath(__file__)) + "/..");
 
@@ -18,17 +22,17 @@ read_prefs(prefs, FIREFOX_HOME + "/user.js/my.js");
 
 for key, value in prefs.items() :
     if ( value == None ) : continue;
-    if value == None : continue
-    if isinstance(value, bool) and value == False :
-        print(f'user_pref("{name}", false);')
-        continue
-    if isinstance(value, bool) and value == True :
-        print(f'user_pref("{name}", true);')
-        continue
-    if isinstance(value, int) :
-        print(f'user_pref("{name}", {value});')
-        continue
-    if isinstance(value, str) :
-        print(f'user_pref("{name}", "{value}");')
-        continue
-    raise Exception(f'unknown type: {type(value)}')
+    value_type = type(value);
+    if value_type == bool and value == False :
+        print(f'user_pref("{key}", false);');
+        continue;
+    if value_type == bool and value == True :
+        print(f'user_pref("{key}", true);');
+        continue;
+    if value_type == int :
+        print(f'user_pref("{key}", {value});');
+        continue;
+    if value_type == str :
+        print(f'user_pref("{key}", "{value}");');
+        continue;
+    raise Exception(f'unknown type: {value_type}')
