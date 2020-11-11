61e9c64945c946da76d8360eaa28eec8bdd0e8a2
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-11-11 13:17:52 +0100

    [sbin] jsonlz4 - impl compress option

--- sbin/jsonlz4.py
+++ sbin/jsonlz4.py
@@ -1,19 +1,41 @@
 #!/bin/python
 
-import argparse;
+import argparse, sys;
 
 parser = argparse.ArgumentParser();
-parser.add_argument("jsonlz4");
+parser.add_argument("-c", "--compress", action="store_true");
+parser.add_argument("-d", "--decompress", action="store_true");
+parser.add_argument("fin", nargs="?", default="-");
+parser.add_argument("fout", nargs="?", default="-");
 args = parser.parse_args();
 
-f = open(args.jsonlz4, "rb");
+if not ( args.compress ^ args.decompress ) :
+    parser.print_help();
+    sys.exit(0);
+
+if args.decompress :
+    # read binary
+    fin = sys.stdin.buffer if args.fin == "-" else open(args.fin, "rb");
+    # write utf-8
+    fout = sys.stdout if args.fout == "-" else open(args.fout, "w");
+
+if args.compress :
+    # read utf-8
+    fin = sys.stdin if args.fin == "-" else open(args.fin, "r", encoding = "utf-8");
+    # write binary
+    fout = sys.stdout.buffer if args.fout == "-" else open(args.fout, "wb");
 
 MAGIC = b"mozLz40\x00";
-if ( f.read(len(MAGIC)) != MAGIC ) :
-    raise Exception(f'invalid MAGIC')
 
 import lz4.block, json;
 
-j = json.loads(lz4.block.decompress(f.read()));
+if args.decompress :
+    if ( fin.read(len(MAGIC)) != MAGIC ) :
+        raise Exception("invalid MAGIC");
+    j = json.loads(lz4.block.decompress(fin.read()))
+    json.dump(j, fout);
 
-print(json.dumps(j));
+if args.compress :
+    j = json.load(fin);
+    fout.write(MAGIC);
+    fout.write(lz4.block.compress(bytes(json.dumps(j), "utf-8")));
