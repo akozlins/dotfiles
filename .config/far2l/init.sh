5c095b40e63499f0cf5f4f95e4bc4021202beca0
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-11-11 11:05:46 +0100

    [far2l] add init.sh - init symbolic links

---
+++ .config/far2l/init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -euf
+
+unset CDPATH
+cd "$(dirname -- "$(readlink -f -- "$0")")" || exit 1
+
+find . -type l | while read link ; do
+    target=$(readlink -f -- "$link")
+    mkdir -pv -- "$target"
+done
