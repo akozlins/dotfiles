c94d0973becd3ec91b7c936fa9158e52bbd5dadf
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-12-17 11:38:34 +0100

    cleanup

--- .config/far2l/init.sh
+++ .config/far2l/init.sh
@@ -4,7 +4,7 @@ set -euf
 unset CDPATH
 cd "$(dirname -- "$(readlink -f -- "$0")")" || exit 1
 
-find . -type l | while read link ; do
+find . -type l | while read -r link ; do
     target=$(readlink -f -- "$link")
     mkdir -pv -- "$target"
 done
--- .config/vlc/sed.sh
+++ .config/vlc/sed.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# remove comments
 sed -i -e '/^#.*/d' vlcrc
+# trim empty lines
 sed -i -e '/^$/N;/^\n$/D' vlcrc
