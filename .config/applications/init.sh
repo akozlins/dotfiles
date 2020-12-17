d1dee6f66bb1bcb5fecbbc5a8986fb30b1d2d66f
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-12-17 11:33:41 +0100

    [apps] write to $XDG_DATA_HOME/applications

--- .config/applications/init.sh
+++ .config/applications/init.sh
@@ -4,8 +4,11 @@ set -euf
 unset CDPATH
 cd "$(dirname -- "$(readlink -f -- "$0")")" || exit 1
 
+APPS="$XDG_DATA_HOME/applications"
+mkdir -pv "$APPS"
+
 find "$(pwd)" -name "*.in" | while read -r fin ; do
-    fout=$(dirname -- "$fin")/$(basename -- "$fin" .in)
+    fout=$APPS/$(basename -- "$fin" .in)
     echo "I [] envsubst > $fout"
     envsubst < "$fin" > "$fout"
 done
