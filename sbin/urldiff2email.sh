ab66fba4b409b2ceb986c300e3f83d4057fcb0bf
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-06-26 12:41:20 +0200

    [sbin] add urldiff2email.sh

---
+++ sbin/urldiff2email.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euf
+
+URL="$1"
+DELAY="$2"
+EMAIL="$3"
+
+NEW="$(mktemp)"
+OLD="$(mktemp)"
+DIFF="$(mktemp)"
+
+cleanup() {
+    rv=$?
+    rm -- "$NEW" "$OLD" "$DIFF"
+    exit $rv
+}
+trap cleanup EXIT
+
+while [ true ] ; do
+    curl "$URL" | html2text --decode-errors=ignore > "$NEW"
+
+    if [ -s "$OLD" ] ; then
+        diff -u "$OLD" "$NEW" > "$DIFF"
+        if [ -s "$DIFF" ]; then
+            cat "$DIFF"
+            if [ -n "$EMAIL" ] ; then
+                cat "$DIFF" | mail -s "DIFF: $URL" "$EMAIL"
+            fi
+        fi
+    fi
+
+    cp -- "$NEW" "$OLD"
+    sleep "$DELAY"
+done
