32d0f5d29fc33c5191ef0a8b905a27eb70c8d6e7
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-08-02 11:16:18 +0200

    [ff] update bookmarks.sh

--- .config/firefox/bookmarks.sh
+++ .config/firefox/bookmarks.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -euf
 
-jsonlz4=$(find "default/bookmarkbackups/" | sort | tail -n 1)
+DIR=$(readlink -f -- "$0")
+DIR=$(dirname -- "$DIR")
+jsonlz4=$(find "$DIR/default/bookmarkbackups/" | sort -r | head -n 1)
 
 jsonlz4.py -d "$jsonlz4" - | ./bookmarks.py | json2yaml.py > bookmarks.yaml
 jsonlz4.py -d "$jsonlz4" - | ./bookmarks.py | jsonlz4.py -c - bookmarks.jsonlz4
