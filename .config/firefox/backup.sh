a871c17384b1fb6fefd2d6a24043d839e56b811e
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-08-02 11:15:12 +0200

    [ff] add backup.sh

---
+++ .config/firefox/backup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euf
+
+IFS="$(printf '\n\t')"
+unset CDPATH
+cd "$(dirname -- "$(readlink -f -- "$0")")" || exit 1
+
+FIREFOX="$XDG_CONFIG_HOME/firefox"
+PROFILE="$XDG_CONFIG_HOME/firefox/default"
+
+
+
+sessionstore="$PROFILE/sessionstore-backups/recovery.jsonlz4"
+jsonlz4.py -d "$sessionstore" - | "$FIREFOX/tabs.py" | jq . > "sessionstore.json.$(hostname)"
+
+LAST=$(find . -name "sessionstore.json.$(hostname).*" | sort -r | head -n 1)
+if ! diff "sessionstore.json.$(hostname)" "$LAST" > /dev/null 2>&1 ; then
+    cp "sessionstore.json.$(hostname)" "sessionstore.json.$(hostname).$(date +%FT%T)"
+fi
+
+
+
+bookmarks=$(find "$PROFILE/bookmarkbackups/" -name "*.jsonlz4" | sort -r | head -n 1)
+jsonlz4.py -d "$bookmarks" - | "$FIREFOX/bookmarks.py" | jq . > "bookmarks.json.$(hostname)"
+
+LAST=$(find . -name "bookmarks.json.$(hostname).*" | sort -r | head -n 1)
+if ! diff "bookmarks.json.$(hostname)" "$LAST" > /dev/null 2>&1 ; then
+    cp "bookmarks.json.$(hostname)" "bookmarks.json.$(hostname).$(date +%FT%T)"
+fi
