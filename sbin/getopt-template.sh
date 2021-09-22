0763cde906a3ce9ff1af8a645528845b68cdf486
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-09-22 08:52:44 +0200

    [sbin] add getopt-template

---
+++ sbin/getopt-template.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euf
+
+! getopt --test > /dev/null
+if [[ "${PIPESTATUS[0]}" -ne 4 ]] ; then
+    >&2 echo "E [$0] getopt is not the enchanced version"
+    exit 1
+fi
+
+OPTIONS="hvo:"
+LONGOPTIONS="help,verbose,output:"
+h=0 v=0 o=-
+
+! OPTS=$(getopt --options="$OPTIONS" --longoptions="$LONGOPTIONS" --name="$0" -- "$@")
+if [[ "${PIPESTATUS[0]}" -ne 0 ]] ; then
+    exit 1
+fi
+
+eval set -- "$OPTS"
+
+while true ; do case "$1" in
+    -h|--help) shift ; ((++h)) ;;
+    -v|--verbose) shift ; ((++v)) ;;
+    -o|--output) shift ; o="$1" ; shift ;;
+    --) shift ; break ;;
+    *) exit 1 ;;
+esac done
+
+echo h=$h
+echo v=$v
+echo o=$o
