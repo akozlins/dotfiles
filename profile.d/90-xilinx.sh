ba49d2816e8b64747aa8e4abad200598a7da8c9e
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-10-09 12:46:21 +0200

    [profile] use vivado 2021.1

--- profile.d/90-xilinx.sh
+++ profile.d/90-xilinx.sh
@@ -4,4 +4,4 @@ if [ -z "${XILINXD_LICENSE_FILE:+x}" ] ; then
     export XILINXD_LICENSE_FILE=
 fi
 
-export VIVADO_ROOTDIR="/opt/xilinx/Vivado/2019.2"
+export VIVADO_ROOTDIR="/opt/xilinx/Vivado/2021.1"
