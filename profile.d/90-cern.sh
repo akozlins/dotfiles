df0337a5fcb2ce7335bb43b836717dcfa98f298e
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-11-21 10:46:34 +0100

    [profile] update geant4/root envs

--- profile.d/90-cern.sh
+++ profile.d/90-cern.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
 
-for MY_GEANT4_SH in \
-    "/usr/local/bin/geant4.sh" \
-    "$HOME/.local/bin/geant4.sh" \
-    "$HOME/.local/geant4.cern.ch/bin/geant4.sh" \
+for MY_GEANT4 in \
+    "$HOME/.local/geant4.cern.ch" \
+    "/opt/geant4" \
+    "/usr/local" \
 ; do
-    if [ -f "$MY_GEANT4_SH" ] && cd "$(dirname -- "$MY_GEANT4_SH")" ; then
-        . "$(readlink -f -- "$MY_GEANT4_SH")"
+    if [ -f "$MY_GEANT4/bin/geant4.sh" ] && cd -- "$MY_GEANT4/bin" ; then
+        source geant4.sh
         cd - > /dev/null
         break
     fi
 done
 
-unset -v MY_GEANT4_SH
+unset -v MY_GEANT4
 
 for MY_ROOTSYS in \
     "$HOME/.local/root.cern.ch" \
+    "/opt/root" \
+    "/usr/local" \
 ; do
     if [ -x "$MY_ROOTSYS/bin/root" ] ; then
         ROOTSYS="$MY_ROOTSYS"
 #        export ROOTSYS
         PATH="$ROOTSYS/bin:$PATH"
+        break
     fi
 done
 
