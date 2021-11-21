a5b68c7dcca004a689ff0afd0649716a37ce1d2f
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-11-21 10:52:45 +0100

    cleanup

--- profile.d/90-rust.sh
+++ profile.d/90-rust.sh
@@ -1,4 +1,6 @@
 #
 
+# <https://doc.rust-lang.org/cargo/reference/environment-variables.html>
+
 # location of where to place all generated artifacts, relative to the current working directory
 export CARGO_TARGET_DIR=".cache"
