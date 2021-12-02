abdd4425ac0b58e5da006b26b446477e8d4e3586
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-12-02 10:29:50 +0100

    [openbox] mv xrdb and xset to .config/xinitrc.d/

---
+++ .config/xinitrc.d/50-xrdb.sh
@@ -0,0 +1,3 @@
+#
+
+xrdb -merge "$DOTFILES/.Xresources"
---
+++ .config/xinitrc.d/50-xset.sh
@@ -0,0 +1,12 @@
+#
+
+# disable bell
+xset b off
+# set autorepeat (delay 200 ms, rate 40)
+xset r rate 200 40
+# set black background
+xsetroot -solid black
+# set screen saver timeout
+xset s 3600
+# enable DPMS off mode
+xset dpms 0 0 3600
