f563379d873ad0587b7da9565596b0d42d93ded5
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-12-15 11:29:31 +0100

    cleanup

--- bin/git-prune.sh
+++ bin/git-prune.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -euf
 
+# <https://git-scm.com/docs/git-gc>
+
 git submodule foreach --recursive "git gc --prune='0 days'"
 git gc --prune='0 days' "$@"
--- rc.d/histdb.zsh
+++ rc.d/histdb.zsh
@@ -5,7 +5,7 @@
 MY_ZSH_HISTDB="$ZSH/custom/plugins/zsh-histdb"
 
 # zsh-histdb
-if [ -n "$ZSH_VERSION" ] && [ -e "$MY_ZSH_HISTDB" ] ; then
+if [ -e "$MY_ZSH_HISTDB" ] ; then
     HISTDB_FILE="$XDG_CACHE_HOME/.zsh_history.histdb.db"
     source "$MY_ZSH_HISTDB/sqlite-history.zsh"
     source "$MY_ZSH_HISTDB/histdb-interactive.zsh"
