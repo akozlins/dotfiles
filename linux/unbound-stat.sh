99bcb673415966f6ae44798b440b36173036aff3
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-12-16 22:55:21 +0100

    [linux] unbound-stat - add docs

--- linux/unbound-stat.sh
+++ linux/unbound-stat.sh
@@ -1,6 +1,15 @@
 #/bin/sh
 set -euf
 
-cat /etc/unbound/unbound.log | awk '{ if($6=="A" && $10==0) print $1 " " $5 " " $9; }' | column -t
+# /etc/unbound/unbound.log
+# - $1 - timestamp
+# - $5 - name
+# - $6 - type
+# - $9 - time to resolve
+# - $10 - from cache
 
-cat /etc/unbound/unbound.log | awk '{ if($6=="A" && $10==0) print $5; }' | sort | uniq -c | sort -r -n | head
+# print forwarded requests
+#cat /etc/unbound/unbound.log | awk '{ if($6=="A" && $10==0) print $1 " " $5 " " $9; }' | column -t
+
+# freq list
+cat /etc/unbound/unbound.log | awk '{ if($6=="A" && $10==0) print $5; }' | sort | uniq -c | sort -r -n | head -n 100
