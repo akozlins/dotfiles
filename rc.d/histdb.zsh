68b361638942016cfd1a05a91aeb863b60dc74d3
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-12-18 12:47:32 +0100

    [rc] histdb - fix sql_escape function
    - [zsh_augosuggest] add history.exit_status = 0 to query

--- rc.d/histdb.zsh
+++ rc.d/histdb.zsh
@@ -17,12 +17,17 @@ MY_ZSH_AUTOSUGGEST="$ZSH/custom/plugins/zsh-autosuggestions"
 if [ -e "$MY_ZSH_AUTOSUGGEST" ] ; then
     source "$MY_ZSH_AUTOSUGGEST/zsh-autosuggestions.zsh"
 
+    sql_escape () {
+        print -r -- ${${@//\'/\'\'}//$'\x00'}
+    }
+
     _zsh_autosuggest_strategy_histdb_top_here() {
         local query="select commands.argv from
 history left join commands on history.command_id = commands.rowid
 left join places on history.place_id = places.rowid
 where places.dir LIKE '$(sql_escape $PWD)%'
 and commands.argv LIKE '$(sql_escape $1)%'
+and history.exit_status = 0
 group by commands.argv order by count(*) desc limit 1"
         suggestion=$(_histdb_query "$query")
     }
