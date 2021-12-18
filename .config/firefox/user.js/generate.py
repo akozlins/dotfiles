050d6939a55f2a13a3588a8d60dc85620e4aaed0
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-12-18 16:26:15 +0100

    [ff] refactor generate.py

--- .config/firefox/user.js/generate.py
+++ .config/firefox/user.js/generate.py
@@ -1,48 +1,61 @@
 #!/bin/python
 
-import os, subprocess, sys
-
-def read_prefs(prefs, f, warn = True) :
-    print(f"DEBUG: read_prefs(\"{f}\")", file = sys.stderr)
-    def user_pref(key, value) :
-        if ( key.startswith("_user.") ) : return
-        if ( warn and not key in prefs and value == None ) :
-            print(f"WARN: user_pref(\"{key}\", null)", file = sys.stderr)
-        if ( warn and key in prefs and prefs[key] == value ) :
-            print(f"INFO: user_pref(\"{key}\", {prefs[key]} -> {value})", file = sys.stderr)
-            return;
-        if ( warn and key in prefs and value != None ) :
-            print(f"WARN: user_pref(\"{key}\", {prefs[key]} -> {value})", file = sys.stderr)
-        prefs[key] = value
-    # js uses false/true and null
-    false = False
-    true = True
-    null = None
-    stream = open(f)
-    # run gcc preprocessor (remove comments, etc.)
-    output = subprocess.check_output("/bin/gcc -E -P -", stdin=stream, shell=True)
-    exec(output)
+import os
+import subprocess
+import sys
 
 FIREFOX_HOME = os.path.normpath(os.path.dirname(os.path.realpath(__file__)) + "/..")
 
-prefs = dict()
-read_prefs(prefs, FIREFOX_HOME + "/ghacks-user.js/user.js", warn = False)
-read_prefs(prefs, FIREFOX_HOME + "/user.js/ghacks.js")
-read_prefs(prefs, FIREFOX_HOME + "/user.js/my.js")
+def read_prefs(prefs : dict[str,str], user_js_fname : str) :
+    print(f"DEBUG: read_prefs(\"{user_js_fname}\")", file = sys.stderr)
+    def user_pref(key, value) :
+        if ( key.startswith("_user.") ) :
+            return
+        if ( key not in prefs and value is None ) :
+            print(f"WARN: user_pref(\"{key}\", null)", file = sys.stderr)
+        if ( key in prefs and prefs[key] == value ) :
+            print(f"INFO: user_pref(\"{key}\", {prefs[key]} -> {value})", file = sys.stderr)
+            return
+        if ( key in prefs and value is not None ) :
+            print(f"WARN: user_pref(\"{key}\", {prefs[key]} -> {value})", file = sys.stderr)
+        prefs[key] = value
+    with open(user_js_fname, encoding = "UTF-8") as user_js :
+        # run gcc preprocessor (remove comments, etc.)
+        output = subprocess.check_output("/bin/gcc -E -P -", stdin=user_js, shell=True)
+        exec(output, {}, {
+            # js uses false/true and null
+            "false" : False,
+            "true" : True,
+            "null" : None,
+            "user_pref" : user_pref,
+            "FIREFOX_HOME" : FIREFOX_HOME,
+        })
 
-for key, value in prefs.items() :
-    if ( value == None ) : continue
-    value_type = type(value)
+def emit_prefs(prefs : dict[str,str]) :
+    for key, value in prefs.items() :
+        if ( value is None ) :
+            continue
 
-    if ( value_type == bool and value == False ) :
-        value = "false"
-    elif ( value_type == bool and value == True ) :
-        value = "true"
-    elif ( value_type == int ) :
-        value = value
-    elif ( value_type == str ) :
-        value = '"' + value + '"'
-    else :
-        raise Exception(f'unknown type: {value_type}')
+        value_type = type(value)
 
-    print(f'user_pref("{key}", {value});')
+        if ( value_type == bool and value is False ) :
+            value = "false"
+        elif ( value_type == bool and value is True ) :
+            value = "true"
+        elif ( value_type == int ) :
+            pass
+        elif ( value_type == str ) :
+            value = '"' + value + '"'
+        else :
+            raise Exception(f'unknown type: {value_type}')
+
+        print(f'user_pref("{key}", {value});')
+
+def main() :
+    prefs : dict[str,str] = {}
+    read_prefs(prefs, FIREFOX_HOME + "/ghacks-user.js/user.js")
+    read_prefs(prefs, FIREFOX_HOME + "/user.js/ghacks.js")
+    read_prefs(prefs, FIREFOX_HOME + "/user.js/my.js")
+    emit_prefs(prefs)
+
+main()
