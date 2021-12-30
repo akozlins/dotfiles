04140c83e8b2c572176c40962ef40febea2080d8
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-12-30 09:13:58 +0100

    [sbin] xephyr-i3 - use xprop to set screen size

--- sbin/xephyr-i3.sh
+++ sbin/xephyr-i3.sh
@@ -3,6 +3,11 @@ set -euf
 
 MY_W=1000
 MY_H=1000
+if [ -x "/bin/xprop" ] ; then
+    WORKAREA=($(/bin/xprop -root ' $0 $1 $2 $3' _NET_WORKAREA))
+    MY_W=$(( (WORKAREA[3] - WORKAREA[1])*3/4 ))
+    MY_H=$(( (WORKAREA[4] - WORKAREA[2])*3/4 ))
+fi
 
 MY_XEPHYR_OPTS=(
     # disable access control restrictions
