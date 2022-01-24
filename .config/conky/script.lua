77f85bf2263fa2dd91fdcc44449a40fbf95ac9d8
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-01-24 21:55:22 +0100

    [conky] refactor net script, show UP ifaces

--- .config/conky/script.lua
+++ .config/conky/script.lua
@@ -2,14 +2,24 @@
 do
     function conky_ip()
         local r = ''
-        local f = io.popen('ip link show up')
+        local f = io.popen('ip -br link show')
         for line in f:lines() do
-            local name = line:match("^[0-9]+: ([^:]+):")
-            if name and name ~= 'lo' then
-                local s = conky_parse(string.format("  %s: ${upspeedf %s} KiB / ${downspeedf %s} KiB", name, name, name))
-                if string.len(r) > 0 then r = r .. '\n' end
-                r = r .. s
+            if not line:match('^[^ ]+ +UP') then
+                goto continue
             end
+
+            local name = line:match('^[^ ]+')
+            if not name
+               or name == 'lo'
+            then
+                goto continue
+            end
+
+            local s = conky_parse(string.format("  %s: ${upspeedf %s} KiB / ${downspeedf %s} KiB", name, name, name))
+            if string.len(r) > 0 then r = r .. '\n' end
+            r = r .. s
+
+            ::continue::
         end
         f:close()
         return r
