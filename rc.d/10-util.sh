162fc7eedab02f8b4dafe6873c3ac4be5ce6607d
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-01-24 22:03:30 +0100

    [rc] add my_mount_luks_fs

--- rc.d/10-util.sh
+++ rc.d/10-util.sh
@@ -17,3 +17,14 @@ my_pacman_diff() {
     file=$(echo "$1" | sed 's|^/||')
     tar --extract --to-stdout --file="/var/cache/pacman/pkg/${pkg}-$(uname -m).pkg.tar.zst" "$file" | diff - "$1"
 }
+
+my_mount_luks_fs () {
+    LUKS_UUID="$1"
+    LUKS_NAME="luks-$LUKS_UUID"
+    cryptsetup open --allow-discards "/dev/disk/by-uuid/$LUKS_UUID" "luks-$LUKS_UUID"
+
+    FS_UUID="$2"
+    fsck.ext4 "/dev/disk/by-uuid/$FS_UUID"
+    mkdir -p -- "/run/media/$USER/$FS_UUID"
+    mount -o "rw,noatime,discard" "/dev/disk/by-uuid/$FS_UUID" "/run/media/$USER/$FS_UUID"
+}
