6b36f26b6431a361f3a77fd64ce75adcdf983b53
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-02-12 11:48:27 +0100

    [conky] update config

--- .config/conky/script.lua
+++ .config/conky/script.lua
@@ -13,6 +13,7 @@ do
             local name = line:match('^[^ ]+')
             if not name
                or name == 'lo'
+               or string.match(name, '^br-')
                or string.match(name, '^veth')
             then
                 goto continue
