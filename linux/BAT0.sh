e86e836554df4d2eb90cdd7099bd98ea9dc3cdbf
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-03-11 14:13:21 +0100

    [linux] update BAT script

--- linux/BAT0.sh
+++ linux/BAT0.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -euf
 
-~/.dotfiles/sbin/BAT-energy.sh 0 >> "BAT0.$(date +%Y).out"
+BAT0=$(acpi -i)
+
+if ! diff -q "BAT0.out~" <(echo "$BAT0") ; then
+    ~/.dotfiles/sbin/BAT-energy.sh 0 >> "BAT0.$(date +%Y).out"
+    echo "$BAT0" > "BAT0.out~"
+fi
