71facf0f77897cef7983287a930ddde3032b18bc
Alexandr Kozlinskiy <>
2022-03-12 15:14:16 +0100

    [borg] add create/prune script

---
+++ .config/borg/start.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -euf
+unset CDPATH
+cd "$(dirname "$(readlink -f "$0")")" || exit 1
+
+source -- .env
+[ -d "$BORG_REPO" ] || exit 1
+
+if true ; then
+    TIME_LAST=$(date +%s -d "$(borg list --format='{time}' --last=1)")
+    TIME_NOW=$(date +%s)
+    TIME_DELTA=$(( (TIME_NOW - TIME_LAST) / 3600 ))
+    if [ "$TIME_DELTA" -lt 23 ] ; then
+        exit 0
+    fi
+fi
+
+PATTERNS="./user.patterns"
+PREFIX="$USER@{hostname}"
+LOG="./.cache/$USER.log"
+
+export BORG_RELOCATED_REPO_ACCESS_IS_OK=no
+export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=no
+
+borg create \
+    --verbose \
+    --list --filter AME \
+    --stats \
+    --exclude-caches \
+    --exclude-if-present .borgexclude --keep-exclude-tags \
+    --pattern "R /home/$USER" \
+    --patterns-from "$PATTERNS" \
+    --one-file-system \
+    ::"$PREFIX-{utcnow:%Y%m%dT%H%M%S}" \
+    2>&1 | tee -a "$LOG"
+
+borg prune \
+    --verbose \
+    --list \
+    --stats \
+    --keep-daily=6 \
+    --keep-weekly=3 \
+    --keep-monthly=-1 \
+    --prefix="$PREFIX-" \
+    2>&1 | tee -a "$LOG"
+
+sync
