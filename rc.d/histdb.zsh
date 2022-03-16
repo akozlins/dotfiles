99995ade6adeac34d1a83575e1b38d492144d544
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-03-16 22:42:35 +0100

    [histdb] use XDG_DATA_HOME

--- rc.d/histdb.zsh
+++ rc.d/histdb.zsh
@@ -6,7 +6,7 @@ MY_ZSH_HISTDB="$ZSH/custom/plugins/zsh-histdb"
 
 # zsh-histdb
 if [ -e "$MY_ZSH_HISTDB" ] ; then
-    HISTDB_FILE="$XDG_CACHE_HOME/.zsh_history.histdb.db"
+    HISTDB_FILE="$XDG_DATA_HOME/.zsh_history.histdb.db"
     source "$MY_ZSH_HISTDB/sqlite-history.zsh"
     source "$MY_ZSH_HISTDB/histdb-interactive.zsh"
     autoload -Uz add-zsh-hook
