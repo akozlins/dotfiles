a06b29077b837b1740d1e33f41e97067c1de53a4
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-03-31 21:02:53 +0200

    [profile] set CARGO_INSTALL_ROOT

--- profile.d/90-rust.sh
+++ profile.d/90-rust.sh
@@ -4,3 +4,5 @@
 
 # location of where to place all generated artifacts, relative to the current working directory
 export CARGO_TARGET_DIR=".cache"
+export CARGO_INSTALL_ROOT="$XDG_DATA_HOME/cargo"
+PATH="$CARGO_INSTALL_ROOT/bin:$PATH"
