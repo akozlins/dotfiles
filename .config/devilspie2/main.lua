dfaf37a2d89aca8055626972cadb13f8c3b23b92
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-05-19 09:38:17 +0200

    [devilspie] update zathura window size

--- .config/devilspie2/main.lua
+++ .config/devilspie2/main.lua
@@ -16,5 +16,5 @@ if ( wclass == "xreader" or wclass == "atril" or wclass == "zathura" ) then
     local x, y, w, h = get_monitor_geometry()
     debug_print("set_window_size")
 --    set_window_size(w*2/3, h*2/3)
-    xywh(w*1/6, h*1/6, w*2/3, h*2/3)
+    xywh(w*1/6, h*1/6, w*2/3, h*3/4)
 end
