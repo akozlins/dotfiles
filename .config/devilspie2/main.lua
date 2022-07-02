ef4b4d3efbffe1723f526a13bb65113aaf3deea1
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-07-02 23:58:08 +0200

    [config] add bakamplayer.ini

--- .config/devilspie2/main.lua
+++ .config/devilspie2/main.lua
@@ -18,3 +18,9 @@ if ( wclass == "xreader" or wclass == "atril" or wclass == "zathura" ) then
 --    set_window_size(w*2/3, h*2/3)
     xywh(w*1/6, h*1/6, w*2/3, h*3/4)
 end
+
+if ( wclass == "baka-mplayer" ) then
+    local x, y, w, h = get_monitor_geometry()
+    debug_print("set_window_size")
+    xywh(0, 0, w*1/2, h*1/2)
+end
