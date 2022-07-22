55fcabbaa24a41a3fe28cb3db634fd54ea749592
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-07-22 16:05:13 +0200

    [bin] add/use my_open_image/video

--- .config/far2l/open.sh
+++ .config/far2l/open.sh
@@ -5,26 +5,25 @@ mime=$(xdg-mime query filetype "$1")
 
 case "$mime" in
     text/*|application/json|inode/x-empty)
-#        [[ "$1" == *.md ]] && command -pv okular &> /dev/null && exec okular "$1"
 #        exec gedit "$1"
         exec xdg-open "$1"
         ;;
     image/*)
-#        exec geeqie "$1"
-        exec xdg-open "$1"
+#        exec xdg-open "$1"
+        exec my_open_image "$1"
         ;;
     audio/*|video/*)
-        exec mpv "$1"
+#        exec xdg-open "$1"
+        exec my_open_video "$1"
         ;;
     application/pdf)
-#        exec evince "$1"
-        exec xdg-open "$1"
+#        exec xdg-open "$1"
+        exec my_open_pdf "$1"
         ;;
     application/postscript)
         exec okular "$1"
         ;;
     application/octet-stream)
-#        exec zenity --info --title="$0" --text="MIME type: '$mime'"
         exec dunstify "MIME type: '$mime'"
         ;;
     application/*)
