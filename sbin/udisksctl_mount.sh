5cfcff3dc504a752c287eb6f5b987c273c49f524
Alexandr Kozlinskiy <akozlins@gmail.com>
2023-01-13 13:43:56 +0100

    [sbin] add udisksctl_mount/umount

---
+++ sbin/udisksctl_mount.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euf
+
+KEY=$3
+
+LUKS=(find /dev/disk/by-uuid/ -maxdepth 1 -name "$1")
+if [[ ${#LUKS[@]} > 1 ]] ; then
+    exit 1
+fi
+udisksctl unlock --block-device "$LUKS" \
+    --key-file "$KEY"
+
+DISK=(find /dev/disk/by-uuid/ -maxdepth 1 -name "$2")
+if [[ ${#LUKS[@]} > 1 ]] ; then
+    udisksctl lock --block-device "$LUKS"
+    exit 1
+fi
+udisksctl mount --block-device "$DISK"
---
+++ sbin/udisksctl_umount.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euf
+
+sync
+
+DISK=(find /dev/disk/by-uuid/ -maxdepth 1 -name "$2")
+if [[ ${#LUKS[@]} > 1 ]] ; then
+    exit 1
+fi
+udisksctl unmount --block-device $MOUNT
+
+LUKS=(find /dev/disk/by-uuid/ -maxdepth 1 -name "$1")
+if [[ ${#LUKS[@]} > 1 ]] ; then
+    exit 1
+fi
+udisksctl lock --block-device $LUKS
+udisksctl power-off --block-device $LUKS
