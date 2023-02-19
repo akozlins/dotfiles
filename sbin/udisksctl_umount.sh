66df886867f608008f42a0271b356280221c0f4b
Alexandr Kozlinskiy <akozlins@gmail.com>
2023-02-19 11:10:13 +0100

    cleanup

--- sbin/udisksctl_umount.sh
+++ sbin/udisksctl_umount.sh
@@ -3,13 +3,13 @@ set -euf
 
 sync
 
-DISK=(find /dev/disk/by-uuid/ -maxdepth 1 -name "$2")
+DISK=$(find /dev/disk/by-uuid/ -maxdepth 1 -name "$2")
 if [[ ${#LUKS[@]} > 1 ]] ; then
     exit 1
 fi
 udisksctl unmount --block-device $MOUNT
 
-LUKS=(find /dev/disk/by-uuid/ -maxdepth 1 -name "$1")
+LUKS=$(find /dev/disk/by-uuid/ -maxdepth 1 -name "$1")
 if [[ ${#LUKS[@]} > 1 ]] ; then
     exit 1
 fi
