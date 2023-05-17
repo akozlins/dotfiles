eb80bfff7043c014a86b2d3ff82619242e2d5862
Alexandr Kozlinskiy <akozlins@gmail.com>
2023-05-17 13:33:38 +0200

    cleanup

--- profile.d/90-xilinx.sh
+++ profile.d/90-xilinx.sh
@@ -4,4 +4,4 @@ if [ -z "${XILINXD_LICENSE_FILE:+x}" ] ; then
     export XILINXD_LICENSE_FILE=
 fi
 
-export VIVADO_ROOTDIR="/opt/xilinx/Vivado/2021.1"
+export XILINX_VIVADO="/opt/xilinx/Vivado/2021.1"
