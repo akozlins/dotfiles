7f67974ce04dda81ec498ba7f2b1e96b5d075027
Alexandr Kozlinskiy <akozlins@gmail.com>
2023-05-20 12:55:26 +0200

    [rc] update atuin

--- rc.d/50-atuin.zsh
+++ rc.d/50-atuin.zsh
@@ -1,6 +1,15 @@
-#
+#!/bin/zsh
+
+# `atuin init zsh --disable-up-arrow`
 
 if command -v atuin &> /dev/null ; then
+    # shellcheck disable=SC2034,SC2153,SC2086,SC2155
+
+    # Above line is because shellcheck doesn't support zsh, per
+    # https://github.com/koalaman/shellcheck/wiki/SC1071, and the ignore: param in
+    # ludeeus/action-shellcheck only supports _directories_, not _files_. So
+    # instead, we manually add any error the shellcheck step finds in the file to
+    # the above line ...
 
     # Source this in your ~/.zshrc
     autoload -U add-zsh-hook
@@ -8,21 +17,21 @@ if command -v atuin &> /dev/null ; then
     export ATUIN_SESSION=$(atuin uuid)
     export ATUIN_HISTORY="atuin history list"
 
-    _atuin_preexec(){
-        local id; id=$(atuin history start -- "$1")
+    _atuin_preexec() {
+        local id
+        id=$(atuin history start -- "$1")
         export ATUIN_HISTORY_ID="$id"
     }
 
-    _atuin_precmd(){
+    _atuin_precmd() {
         local EXIT="$?"
 
         [[ -z "${ATUIN_HISTORY_ID}" ]] && return
 
-
-        (RUST_LOG=error atuin history end --exit $EXIT -- $ATUIN_HISTORY_ID &) > /dev/null 2>&1
+        (RUST_LOG=error atuin history end --exit $EXIT -- $ATUIN_HISTORY_ID &) >/dev/null 2>&1
     }
 
-    _atuin_search(){
+    _atuin_search() {
         emulate -L zsh
         zle -I
 
@@ -30,28 +39,28 @@ if command -v atuin &> /dev/null ; then
         echoti rmkx
         # swap stderr and stdout, so that the tui stuff works
         # TODO: not this
-        output=$(RUST_LOG=error atuin search -i -- $BUFFER 3>&1 1>&2 2>&3)
+        # shellcheck disable=SC2048
+        output=$(RUST_LOG=error atuin search $* -i -- $BUFFER 3>&1 1>&2 2>&3)
         echoti smkx
 
-        if [[ -n $output ]] ; then
-                RBUFFER=""
-                LBUFFER=$output
+        if [[ -n $output ]]; then
+            RBUFFER=""
+            LBUFFER=$output
         fi
 
         zle reset-prompt
+    }
+
+    _atuin_up_search() {
+        _atuin_search --shell-up-key-binding
     }
 
     add-zsh-hook preexec _atuin_preexec
     add-zsh-hook precmd _atuin_precmd
 
     zle -N _atuin_search_widget _atuin_search
+    zle -N _atuin_up_search_widget _atuin_up_search
 
-    if [[ -z $ATUIN_NOBIND ]]; then
-        bindkey '^r' _atuin_search_widget
-
-        # depends on terminal mode
-        #bindkey '^[[A' _atuin_search_widget
-        #bindkey '^[OA' _atuin_search_widget
-    fi
+    bindkey '^r' _atuin_search_widget
 
 fi
