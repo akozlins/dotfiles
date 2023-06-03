e6bee8311704f3921db0e9bcb4bfdf279f545f56
Alexandr Kozlinskiy <akozlins@gmail.com>
2023-06-03 09:33:41 +0200

    [code] mv init to install.d/code.sh

---
+++ install.d/code.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for f in \
+    "code/User/settings.json" \
+    "code/User/keybindings.json" \
+    "code-server/User/settings.json" \
+    "code-server/User/keybindings.json" \
+; do
+    [ -f "$XDG_DATA_HOME/$f" ] && continue
+    mkdir -p -- "$(dirname -- "$XDG_DATA_HOME/$f")"
+    ln -s -T "$XDG_CONFIG_HOME/$f" "$XDG_DATA_HOME/$f"
+done
