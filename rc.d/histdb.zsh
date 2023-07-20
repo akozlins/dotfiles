a9ca6c1adf0a69090b0a180e508c8ee9ce0046d6
Alexandr Kozlinskiy <akozlins@gmail.com>
2023-07-20 15:10:01 +0200

    [rc] remove zsh autosuggest plugin

--- rc.d/histdb.zsh
+++ rc.d/histdb.zsh
@@ -11,23 +11,3 @@ if [ -e "$MY_ZSH_HISTDB" ] ; then
     source "$MY_ZSH_HISTDB/histdb-interactive.zsh"
     autoload -Uz add-zsh-hook
 fi
-
-MY_ZSH_AUTOSUGGEST="$ZSH/custom/plugins/zsh-autosuggestions"
-
-if [ -e "$MY_ZSH_AUTOSUGGEST" ] ; then
-    source "$MY_ZSH_AUTOSUGGEST/zsh-autosuggestions.zsh"
-
-    _zsh_autosuggest_strategy_histdb_top_here() {
-        local query="select commands.argv from
-history left join commands on history.command_id = commands.rowid
-left join places on history.place_id = places.rowid
-where places.dir LIKE '$(sql_escape $PWD)%'
-and commands.argv LIKE '$(sql_escape $1)%'
-and history.exit_status = 0
-group by commands.argv order by count(*) desc limit 1"
-        suggestion=$(_histdb_query "$query")
-    }
-
-    ZSH_AUTOSUGGEST_STRATEGY=histdb_top_here
-    ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#888888"
-fi
