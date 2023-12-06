430b8d4cd22b291d7d40b05a16e3f26c330c2ddb
Alexandr Kozlinskiy <akozlins@gmail.com>
2023-12-06 14:24:15 +0100

    [code] add tasks.json

--- install.d/code.sh
+++ install.d/code.sh
@@ -3,8 +3,10 @@
 for f in \
     "code/User/settings.json" \
     "code/User/keybindings.json" \
+    "code/User/tasks.json" \
     "code-server/User/settings.json" \
     "code-server/User/keybindings.json" \
+    "code-server/User/tasks.json" \
 ; do
     [ -f "$XDG_DATA_HOME/$f" ] && continue
     mkdir -p -- "$(dirname -- "$XDG_DATA_HOME/$f")"
