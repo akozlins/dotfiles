81471d2e6d89218aedc9123f3632868961f6be43
Alexandr Kozlinskiy <akozlins@gmail.com>
2023-12-06 14:22:43 +0100

    [linux] update iptables-stat.sh

--- linux/iptables-stat.sh
+++ linux/iptables-stat.sh
@@ -9,16 +9,17 @@ fi
 exec \
 iptables -nvL \
 | awk '
+# report policy DROP
 ($3 == "(policy" && "DROP" == $4 && $5 != "0") {
     print $0
 }
-($1 != "0" && $3 == "ACCEPT" && $6 != $7) {
-    print $0
-}
-($1 != "0" && $3 == "DROP") {
-    print $0
-}
-($1 != "0" && $3 == "REJECT") {
+
+# skip 0 packets/bytes
+($1 == "0") { next }
+# skip ACCEPT on same interface
+($3 == "ACCEPT" && $6 == $7) { next }
+
+($3 == "ACCEPT" || $3 == "DROP" || $3 == "LOG" || $3 == "REJECT") {
     print $0
 }
 '
