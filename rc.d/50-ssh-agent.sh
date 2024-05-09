3824efc170b2fc84b153b5b0c90a142faceb1dc3
Alexandr Kozlinskiy <akozlins@gmail.com>
2024-05-09 11:42:16 +0200

    [rc] fix ssh-agent service check

--- rc.d/50-ssh-agent.sh
+++ rc.d/50-ssh-agent.sh
@@ -1,7 +1,7 @@
 #
 
 if [ -z "$SSH_CONNECTION" ] ; then
-    systemctl --user status ssh-agent.service &>/dev/null || true
+    systemctl --user status ssh-agent.service &>/dev/null
     # check return code for 'unit is not active'
     [[ $? == 3 ]] && systemctl --user start ssh-agent.service \
     && export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
