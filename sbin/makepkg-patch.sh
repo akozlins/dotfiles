1912a7704e4d7fc2d58fa5669cecaff2ca98414e
Alexandr Kozlinskiy <akozlins@gmail.com>
2024-12-30 12:12:35 +0100

    update makepkg patches

--- sbin/makepkg-patch.sh
+++ sbin/makepkg-patch.sh
@@ -6,19 +6,23 @@ PKG=$1
 mkdir -p -- "$XDG_CACHE_HOME/makepkg"
 cd -- "$XDG_CACHE_HOME/makepkg"
 
-#git clone --branch "packages/$PKG" --single-branch https://github.com/archlinux/svntogit-packages.git
-yay --getpkgbuild -- "$PKG"
+[ -d "$PKG" ] || git clone "https://gitlab.archlinux.org/archlinux/packaging/packages/$PKG.git"
+#yay --getpkgbuild -- "$PKG"
 cd -- "$PKG"
 
 git stash
+git pull
 if [ -r "$XDG_CONFIG_HOME/makepkg/$PKG.patch" ] ; then
     git apply --verbose "$XDG_CONFIG_HOME/makepkg/$PKG.patch"
 fi
 
 makepkg -s --nobuild --cleanbuild
-[ -d "$XDG_CONFIG_HOME/makepkg/$PKG" ] && find "$XDG_CONFIG_HOME/makepkg/$PKG" -name '*.patch' | while read -r p ; do
-    patch --forward --strip=1 --input="$f"
+find "$XDG_CONFIG_HOME/makepkg/$PKG" -name '*.patch' 2>/dev/null \
+| while read -r p ; do
+    # TODO: use scripts instead of patches
+    patch --forward --strip=1 --input="$p"
 done
 makepkg --noextract -f
 
 makepkg --install
+#yay -U ...
