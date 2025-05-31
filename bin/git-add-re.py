239b17781e8db1e09b5684db2ba00f9b6f7a4199
Alexandr Kozlinskiy <akozlins@gmail.com>
2025-05-31 12:17:12 +0200

    [bin] refactor git-add-re

--- bin/git-add-re.py
+++ bin/git-add-re.py
@@ -3,46 +3,49 @@
 import argparse
 import os
 import re
-import subprocess
+import sys
 import tempfile
 
 parser = argparse.ArgumentParser()
-parser.add_argument("pattern", nargs="?", default="")
+parser.add_argument('pattern', nargs='?', default='')
 args = parser.parse_args()
 
-def fix_hunk(fname) :
-    with open(fname, "r") as f :
+def fix_hunk(fname:str) -> None:
+    with open(fname, 'r') as f :
         lines = f.read().splitlines(keepends=True)
-    nBefore = nAfter = 0
+
+    n_before = n_after = 0
     # get before/after counts
     for line in reversed(lines) :
         if line.startswith('@@') : break
         if line.startswith(' ') :
-            nBefore += 1
-            nAfter += 1
-        if line.startswith('-') : nBefore += 1
-        if line.startswith('+') : nAfter += 1
-    with open(fname, "w") as f :
-        for line in lines :
-            if line.startswith('@@') :
-                # update before/after counts
-                block = re.match(r'^@@ [-](\d+),\d+ [+](\d+),\d+ @@(.*)$', line).groups()
-                line = f'@@ -{block[0]},{nBefore} +{block[1]},{nAfter} @@{block[2]}\n'
-            f.write(line)
-        f.flush()
+            n_before += 1
+            n_after += 1
+        if line.startswith('-') : n_before += 1
+        if line.startswith('+') : n_after += 1
 
-def split_hunk(fname) :
-    with open(fname, "r") as f :
+    f = open(fname, 'w')
+    for line in lines :
+        if line.startswith('@@') :
+            # update before/after counts
+            block = re.match(r'^@@ [-](\d+),\d+ [+](\d+),\d+ @@(.*)$', line).groups()
+            line = f'@@ -{block[0]},{n_before} +{block[1]},{n_after} @@{block[2]}\n'
+        f.write(line)
+    f.flush()
+    f.close()
+
+def split_hunk(fname:str, all:bool=False) -> [str]:
+    with open(fname, 'r') as f :
         lines = f.read().splitlines(keepends=True)
     state = ''
     header = ''
     block = None
     hunk = []
     for line in lines :
-        if line.startswith("diff ") :
+        if line.startswith('diff ') :
             state = 'header'
             header = ''
-        if line.startswith("@@ ") :
+        if line.startswith('@@ ') :
             state = 'hunk'
             block = re.match(r'^@@ [-](\d+),\d+ [+](\d+),\d+ @@(.*)$', line).groups()
             continue
@@ -80,42 +83,44 @@ def split_hunk(fname) :
     n = int(ctx[0]/2)
     hunks.append(header
         + f'@@ -{block[0]},0 +{block[1]},0 @@ {block[2]}\n'
-        + ''.join(hunk[:ctx[1]+n])
+        + ''.join(hunk[:ctx[1]+n]),
     )
     hunks.append(header
         + f'@@ -{ctx[2]+n},0 +{ctx[3]+n},0 @@ {block[2]}\n'
-        + ''.join(hunk[ctx[1]+n:])
+        + ''.join(hunk[ctx[1]+n:]),
     )
 
     return hunks
 
-def do_hunk(fname) :
+def do_hunk(fname:str) -> bool:
     # print colored diff
-    os.system(f'colordiff < {fname}')
+    fix_hunk(fname)
+    os.system(f'colordiff < "{fname}"')
     # ask user what to do
     try : i = input('Stage this hunk [y,n,q,s,e,?]? ').lower()
-    except KeyboardInterrupt : return exit()
+    except KeyboardInterrupt : return sys.exit()
     print('')
     match i :
         case 'y' : None
         case 'n' : return True
-        case 'q' : exit()
+        case 'q' : sys.exit()
+        case 'd' : return False # TODO: skip file
         case 's' :
             hunks = split_hunk(fname)
-            if len(hunks) < 2 : return False
+            if len(hunks) == 1 : return False
             do_hunks(hunks)
             return True
         case 'e' :
             #os.system(f'nano --syntax=patch +1,1 {fname}')
-            os.system(f'$EDITOR -- {fname}')
+            os.system(f'$EDITOR -- "{fname}"')
         case _ : return False
-    fix_hunk(fname)
-    
+
     # apply patch
-    os.system(f'git apply --cached {fname}')
+    fix_hunk(fname)
+    os.system(f'git apply --cached --unidiff-zero -- "{fname}"')
     return True
 
-def do_hunks(hunks) :
+def do_hunks(hunks:[str]) -> None:
     for hunk in hunks :
         with tempfile.NamedTemporaryFile(suffix='.patch', mode='w', delete_on_close=False) as f :
             f.write(hunk)
@@ -128,10 +133,10 @@ hunks = []
 diff = os.popen(f'/usr/bin/git diff | grepdiff --only-match=mod --output-matching=hunk "{args.pattern}"').read()
 #for hunk in re.split(r'(^diff .*)$', diff, flags=re.MULTILINE|re.DOTALL) :
 for line in diff.splitlines(keepends=True) :
-    if line.startswith("diff ") :
+    if line.startswith('diff ') :
         state = 'header'
         header = ''
-    if line.startswith("@@ ") :
+    if line.startswith('@@ ') :
         state = 'hunk'
         hunks.append(header)
     if state == 'header' :
