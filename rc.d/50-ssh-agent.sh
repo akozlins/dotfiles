6090b6c78464484b0c65e7c4d97ad5141b895fc3
akozlins <akozlins@archlinux>
2025-11-18 10:30:13 +0000

    [rc] fix ssh-agent startup

--- rc.d/50-ssh-agent.sh
+++ rc.d/50-ssh-agent.sh
@@ -5,7 +5,7 @@ if [ -z "$SSH_CONNECTION" ] \
 && [ -r /usr/bin/systemctl ] ; then
     /usr/bin/systemctl --user status ssh-agent.service &> /dev/null
     # check unit is active or try to start
-    if [[ $? == 0 ]] || systemctl --user start ssh-agent.service ; then
+    if [[ $? == 0 ]] || [[ $? == 3 ]] && systemctl --user start ssh-agent.service ; then
         alias ssh="SSH_AUTH_SOCK='$XDG_RUNTIME_DIR/ssh-agent.socket' ssh"
         #export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
     fi
