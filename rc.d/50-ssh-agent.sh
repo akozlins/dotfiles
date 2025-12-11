166fdb6c2233d598410154ceaa4380af427e37c5
Alexandr Kozlinskiy <akozlins@gmail.com>
2025-12-11 09:27:03 +0100

    [rx] fix ssh-agent status check

--- rc.d/50-ssh-agent.sh
+++ rc.d/50-ssh-agent.sh
@@ -5,8 +5,10 @@ if [ -z "$SSH_CONNECTION" ] \
 && [ -r /usr/bin/systemctl ] ; then
     /usr/bin/systemctl --user status ssh-agent.service &> /dev/null
     # check unit is active or try to start
-    if [[ $? == 0 ]] || [[ $? == 3 ]] && systemctl --user start ssh-agent.service ; then
+    status_ssh_agent=$?
+    if [[ $status_ssh_agent == 0 ]] || [[ $status_ssh_agent == 3 ]] && systemctl --user start ssh-agent.service ; then
         alias ssh="SSH_AUTH_SOCK='$XDG_RUNTIME_DIR/ssh-agent.socket' ssh"
         #export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
     fi
+    unset status_ssh_agent
 fi
